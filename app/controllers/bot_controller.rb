5c25baef3bddd0133d8f0661ede5f56428402925
snowflakekiller <karuna.skj@gmail.com>
2016-07-05 14:14:15 +0800

    added response handling

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,5 +1,16 @@
 class BotController < ActionController::Base
   def facebook
-    
+    query = params[:result]
+    city = query[:parameters]['geo-city']
+    response =
+            {
+              "speech": "#{city} is a nice place",
+              "displayText": "#{city} is a nice place",
+              "data": "",
+              "contextOut": [{"name":"weather", "lifespan":2, "parameters":{"city":"#{city}"}}],
+              "source": "Chatbottest"
+            }
+
+    render json: response
   end
 end
